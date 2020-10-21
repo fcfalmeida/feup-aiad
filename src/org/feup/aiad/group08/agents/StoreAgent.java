0cd9e56538fb009643d00498c53364258e2d8fe5
Ana Santos <32843618+anamafalda98@users.noreply.github.com>
2020-10-21 12:45:41 +0100

    Create StoreAgent.java

---
+++ src/org/feup/aiad/group08/agents/StoreAgent.java
@@ -0,0 +1,19 @@
+package org.feup.aiad.group08.agents;
+
+import jade.core.Agent;
+import jade.core.behaviours.Behaviour;
+import java.util.Vector;
+
+public class StoreAgent extends Agent {
+    /**
+     *
+     */
+    private static final long serialVersionUID = -3205276776739404040L;
+    private float balanceAvailable;
+    private int stock;
+    private int sale;
+    private float price;
+    private float finalPrice;
+
+    
+}
