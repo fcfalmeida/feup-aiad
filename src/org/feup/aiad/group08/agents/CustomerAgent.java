ae431bb5250047efe836058166443ee2915ba06f
RicardoCQB <ricardo_cqb@hotmail.com>
2020-10-24 14:35:54 +0100

    Added influenceability generator

--- src/org/feup/aiad/group08/agents/CustomerAgent.java
+++ src/org/feup/aiad/group08/agents/CustomerAgent.java
@@ -1,5 +1,6 @@
 package org.feup.aiad.group08.agents;
 
+import java.util.Random;
 import jade.core.Agent;
 import jade.core.behaviours.Behaviour;
 import java.util.Vector;
@@ -7,14 +8,18 @@ import java.util.Vector;
 public class CustomerAgent extends Agent {
 
     private static final long serialVersionUID = -8345978142167560058L;
+    private static final int INFLUENCE_UPPER_LIMIT = 10;
+    private static final int INFLUENCE_LOWER_LIMIT = 1;
 
-    private float balance;
-    private float influentiability;
+    private float balance; // The total funds availaible for the customer to purchase.
+    private double influenceability; // Each agent can be more easily or not influenced by promotions
     private Vector<String> storePreferences;
 
     public CustomerAgent(float initBalance, Vector<String> storePreferences) {
         balance = initBalance;
         this.storePreferences = storePreferences;
+        influenceability = generateInfluenceability();
+        
     }
 
     public float getBalance() {
@@ -23,5 +28,13 @@ public class CustomerAgent extends Agent {
 
     public Vector<String> getStorePreferences() {
         return storePreferences;
+    }
+
+    public double getInfluenceability(){
+        return influenceability;
+    }
+        
+    private double generateInfluenceability(){
+        return new Random().nextGaussian() * INFLUENCE_UPPER_LIMIT + INFLUENCE_LOWER_LIMIT;
     }
 }
