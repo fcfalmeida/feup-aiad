21d514d5ab90c43c6090bd930a2dd9d56b66e61e
RicardoCQB <ricardo_cqb@hotmail.com>
2020-10-24 16:03:41 +0100

    Implemented CustomerUnitTests

---
+++ src/org/feup/aiad/unit/testing/CustomerUnitTests.java
@@ -0,0 +1,28 @@
+package org.feup.aiad.unit.testing;
+
+import org.junit.jupiter.api.Test;
+
+import jade.wrapper.StaleProxyException;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.Vector;
+import org.feup.aiad.group08.agents.CustomerAgent;
+
+public class CustomerUnitTests {
+
+    public static void main(String[] args) {        
+        
+        CustomerAgent customer = new CustomerAgent();
+        double influenceability = customer.getInfluenceability(); 
+        System.out.println(influenceability);    
+    }       
+        
+    
+    @Test
+    public void testGenerateInfluenceability(){
+        CustomerAgent customer = new CustomerAgent();
+        double influenceability = customer.getInfluenceability();         
+        assertTrue(influenceability >= 1 && influenceability <= 10, "Customer generateInfluenceability is not in the expected range.");
+    }
+}
