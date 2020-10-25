1f03d1f1580f6f936d2c2c3cfdc3800e5ecf6649
Francisco Almeida <franciscocfalmeida@gmail.com>
2020-10-25 15:09:45 +0000

    add DFUserAgent

---
+++ src/org/feup/aiad/group08/agents/DFUserAgent.java
@@ -0,0 +1,105 @@
+package org.feup.aiad.group08.agents;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.feup.aiad.group08.definitions.SystemRole;
+
+import jade.core.AID;
+import jade.core.Agent;
+import jade.domain.DFService;
+import jade.domain.FIPAException;
+import jade.domain.FIPAAgentManagement.DFAgentDescription;
+import jade.domain.FIPAAgentManagement.ServiceDescription;
+
+public class DFUserAgent extends Agent {
+
+    private static final long serialVersionUID = -3233593926341881240L;
+
+    private List<SystemRole> SystemRoles = new ArrayList<>();
+
+    protected void addSystemRole(SystemRole SystemRole) {
+        SystemRoles.add(SystemRole);
+    }
+    
+    @Override
+    protected void setup() {
+        DFAgentDescription ad = new DFAgentDescription();
+        ad.setName(getAID());
+
+        for (SystemRole st : SystemRoles) {
+            ServiceDescription sd = new ServiceDescription();
+            sd.setType(st.toString());
+            sd.setName(getLocalName());
+            ad.addServices(sd);
+        }
+
+        try {
+            DFService.register(this, ad);
+            System.out.println(getName() + " registered in the DF");
+        } catch (FIPAException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    protected void takeDown() {
+        try {
+            DFService.deregister(this);
+        } catch (FIPAException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Searches the DF for agents that provide all of the specified services
+     * @param systemRoles list of system roles (service types) to search
+     * @return list of AIDs
+     */
+    protected List<AID> search(SystemRole... systemRoles) {
+        List<AID> results = new ArrayList<>();
+
+        DFAgentDescription template = new DFAgentDescription();
+        
+        for (SystemRole sr : systemRoles) {
+            ServiceDescription sd = new ServiceDescription();
+            sd.setType(sr.toString());
+            template.addServices(sd);
+
+            try {
+                DFAgentDescription[] result = DFService.search(this, template);
+
+                for (DFAgentDescription ad : result)
+                    results.add(ad.getName());
+            } catch (FIPAException e) {
+                e.printStackTrace();
+            }
+        }
+        
+        return results;
+    }
+    
+    /**
+     * Similar to search but returns only the first agent found
+     * @param systemRoles list of system roles (service types) to search
+     * @return the agent's AID or null if it couldn't be found
+     */
+    protected AID searchOne(SystemRole... systemRoles) {
+        DFAgentDescription template = new DFAgentDescription();
+        
+        for (SystemRole sr : systemRoles) {
+            ServiceDescription sd = new ServiceDescription();
+            sd.setType(sr.toString());
+            template.addServices(sd);
+
+            try {
+                DFAgentDescription[] result = DFService.search(this, template);
+                return result[0].getName();
+            } catch (FIPAException e) {
+                e.printStackTrace();
+            }
+        }
+
+        return null;
+    }
+}
