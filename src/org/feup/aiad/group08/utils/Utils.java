89b8a57ce5fefaad440c6d13c626544b3fa8cbdb
Francisco Almeida <franciscocfalmeida@gmail.com>
2020-10-25 15:11:41 +0000

    add utils package

---
+++ src/org/feup/aiad/group08/utils/Utils.java
@@ -0,0 +1,12 @@
+package org.feup.aiad.group08.utils;
+
+public class Utils {
+
+    public static void sleep(int millis) {
+        try {
+            Thread.sleep(millis);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
