d69525be670f61b5a9acdb55e7bc7250a7b6f93e
Ricardo Brioso <ricardo_cqb@hotmail.com>
2020-10-31 12:15:10 +0000

    Created csvReader.java

---
+++ src/org/feup/aiad/group08/csvManager/csvReader.java
@@ -0,0 +1,44 @@
+package org.feup.aiad.group08.csvManager;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.List;
+import java.util.Arrays;
+
+public class csvReader {
+
+    private static String fileToReadName;
+    private static String delimiter;
+    private List<List<String>> data;
+
+    /**
+     * This constructor reads every line of the csv and stores the values of the line in a list.
+     * Each line represented by a list is added to the data list
+     * @param fileToReadName - Name of the csv file that is going to be read, e.g. "customers.csv"
+     * @param delimiter - Character or string that delimits each value in the lines of the csv
+     * @throws IOException
+     */
+    public csvReader(String fileToReadName, String delimiter) throws IOException{
+
+        this.fileToReadName = fileToReadName;
+        this.delimiter = delimiter;
+
+        try ( BufferedReader buffReader = new BufferedReader( new FileReader(fileToReadName))) {
+            String line;
+            while (( line = buffReader.readLine()) != null) {
+                String[] lineValues = line.split(delimiter);
+                data.add(Arrays.asList(lineValues));
+            }
+        }
+    }
+
+    public String getFileToReadName(){
+        return fileToReadName;
+    }
+    
+    public String getDelimiter(){
+        return delimiter;
+    }    
+}
