2ecf2c1b6134c3c0cc7bee4f3c7ca6a1edd61950
Ana Santos <32843618+anamafalda98@users.noreply.github.com>
2020-11-10 13:10:00 +0000

    Update StoreParser.java

--- src/org/feup/aiad/group08/csvManager/parsers/StoreParser.java
+++ src/org/feup/aiad/group08/csvManager/parsers/StoreParser.java
@@ -3,14 +3,23 @@ package org.feup.aiad.group08.csvManager.parsers;
 import java.util.List;
 
 import org.feup.aiad.group08.agents.StoreAgent;
+import org.feup.aiad.group08.definitions.StoreType;
 
 public class StoreParser implements Parser<StoreAgent> {
 
     @Override
     public StoreAgent parseLine(List<String> line) {
-        // int balance = line.get(0);
 
-        // return new StoreAgent(type, stockCapacity, initBalance, minProfitMargin, desiredProfitMargin)
+        String storeName = line.get(0);
+        StoreType storeType = StoreType.valueOf(line.get(1)); 
+        int stockCapacity = Integer.parseInt(line.get(2));
+        float initBalance = Float.valueOf(line.get(3));
+        float minProfitMargin = Float.valueOf(line.get(4));
+        float desiredProfitMargin = Float.valueOf(line.get(5));
+
+        
+
+        return new StoreAgent(storeName, storeType, stockCapacity, initBalance, minProfitMargin, desiredProfitMargin);
 
         return null;
     }
